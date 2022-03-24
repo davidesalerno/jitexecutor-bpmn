fd5d4e7eae700bdf41fcc23cb299036a00fb64c8
Davide Salerno <dsalerno@redhat.com>
2022-03-24 16:24:02 +0100

    Added support for DRY_RUN in install bash script
    Signed-off-by: Davide Salerno <dsalerno@redhat.com>

--- scripts/install-kubernetes.sh
+++ scripts/install-kubernetes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function install() {
    echo "Deploying jitexecutor"
-   kubectl apply -f "${TARGET_DIR}/jitexecutor.yaml" -n ${PROJECT_NAME}
+   $cmd apply -f "${TARGET_DIR}/jitexecutor.yaml" -n ${PROJECT_NAME}
 
 }
